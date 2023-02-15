5190335411a0a3a7af780104c4e0e6a655900fef
pyama <pyama@pepabo.com>
2023-02-15 12:35:04 +0900

    fix json

--- go.mod
+++ go.mod
@@ -9,7 +9,6 @@ require (
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/google/uuid v1.1.2
 	github.com/gorilla/securecookie v1.1.1
-	github.com/k0kubun/pp v3.0.1+incompatible
 	github.com/labstack/echo/v4 v4.10.0
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/patrickmn/go-cache v2.1.0+incompatible
@@ -26,7 +25,6 @@ require (
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
-	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
--- waitingroom/utils_test.go
+++ waitingroom/utils_test.go
@@ -1,10 +1,10 @@
 package waitingroom
 
 import (
+	"bytes"
+	"encoding/json"
 	"io/ioutil"
 	"net/http/httptest"
-	"net/url"
-	"strings"
 
 	"github.com/labstack/echo/v4"
 	"github.com/sirupsen/logrus"
@@ -15,13 +15,10 @@ func init() {
 }
 func testContext(path, method string, params map[string]string) (echo.Context, *httptest.ResponseRecorder) {
 	rec := httptest.NewRecorder()
-	values := make(url.Values)
-	for k, v := range params {
-		values.Add(k, v)
-	}
+	postBody, _ := json.Marshal(params)
 
-	req := httptest.NewRequest(method, path, strings.NewReader(values.Encode()))
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req := httptest.NewRequest(method, path, bytes.NewBuffer(postBody))
+	req.Header.Set("Content-Type", "application/json")
 	e := echo.New()
 	ctx := e.NewContext(req, rec)
 	return ctx, rec
