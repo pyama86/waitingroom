0d77f7dc5cab2afd2755ce8f410d09c9a2d45d2b
pyama <pyama@pepabo.com>
2023-03-01 21:16:57 +0900

    support old redis

--- waitingroom/queue_access_controller.go
+++ waitingroom/queue_access_controller.go
@@ -73,7 +73,7 @@ func (a *AccessController) Do(ctx context.Context, e *echo.Echo) error {
 			continue
 		}
 
-		ok, err := a.redisClient.SetNX(ctx, a.lockAllowNoKey(m), "1", redis.KeepTTL).Result()
+		ok, err := a.redisClient.SetNX(ctx, a.lockAllowNoKey(m), "1", 0).Result()
 		if err != nil {
 			e.Logger.Warnf("can't set nx %s %s", m, err)
 			return err
